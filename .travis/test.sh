463128531457541d16fbc41bef7436a950512e9a
Buck Golemon <buck@yelp.com>
2014-10-01 16:53:39 -0700

    add pre-commit pylint; factor out lint.sh

--- .travis/test.sh
+++ .travis/test.sh
@@ -10,4 +10,4 @@ py.test --version
 coverage erase
 coverage run --rcfile=$TOP/.coveragerc \
     -m pytest "$@" $TOP/tests $SITEPACKAGES/$PROJECT
-coverage report --show-missing --fail-under 25  # FIXME: should be 100
+coverage report --show-missing --fail-under 27  # FIXME: should be 100
