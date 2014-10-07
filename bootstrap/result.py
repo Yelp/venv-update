a253fd238926452b28741265722ccedaea94b599
Buck Golemon <buck@yelp.com>
2014-10-07 15:52:58 -0700

    improve upgrade behavior; add colorize

--- bootstrap/result.py
+++ bootstrap/result.py
@@ -1842,7 +1842,6 @@ def create_bootstrap_script(extra_text, python_version=''):
 
 import os
 from os.path import exists, getmtime, join
-import subprocess
 import sys
 
 VENV_REQ = 'requirements.d/venv-update.txt'
@@ -1853,16 +1852,16 @@ orig_args = None
 
 
 def extend_parser(dummy_parser):
-    """This callback is called before anything happens.
+    """This runs before anything happens.
     We use it to store the original arguments and insert our own.
     """
     global orig_args  # pylint:disable=global-statement
-    orig_args = sys.argv[1:]
+    orig_args = tuple(sys.argv[1:])
 
     if exists(VENV_UPDATE) and getmtime(VENV_UPDATE) > getmtime(__file__):
         # TODO: use an md5sum rather than mtime
         # TODO: keep project's VENV_HOME's separate
-        os.execv(VENV_UPDATE, [VENV_UPDATE] + orig_args)
+        return after_install()
 
     if hasattr(sys, 'real_prefix'):
         exit('You seem to be running from inside a virtualenv. Please deactivate and try again.')
@@ -1870,12 +1869,20 @@ def extend_parser(dummy_parser):
     sys.argv[1:] = [VENV_HOME]
 
 
+def colorize(cmd):
+    from subprocess import check_call
+    from pipes import quote
+    print '\033[01;36m>\033[m \033[01;33m{0}\033[m'.format(
+        ' '.join(quote(arg) for arg in cmd)
+    )
+    check_call(cmd)
+
+
 def adjust_options(dummy_options, dummy_args):
-    """This callback is called just after parsing.
+    """This runs just after option parsing.
     We use it to ensure that the VENV_HOME doesn't exist before we install to it.
     """
-    from subprocess import check_call
-    check_call(['rm', '-rf', VENV_HOME])
+    colorize(('rm', '-rf', VENV_HOME))
 
 
 def activate(venv):
@@ -1886,8 +1893,8 @@ def activate(venv):
     exec(code, {'__file__': activate_this})  # pylint:disable=exec-used
 
 
-def after_install(dummy_options, dummy_home_dir):
-    """This callback is called just after installing the virtualenv.
+def after_install(dummy_options=None, dummy_home_dir=None):
+    """This runs just after installing the virtualenv.
     We use it to install venv-update, and run it with the original arguments.
     """
     if exists(VENV_REQ):
@@ -1895,13 +1902,17 @@ def after_install(dummy_options, dummy_home_dir):
     else:
         spec = ('venv-update',)
 
-    subprocess.check_call(
-        ('time', join(VENV_HOME, 'bin', 'pip'), 'install') + spec
-    )
-
     activate(VENV_HOME)
+    colorize(('time', 'pip', 'install', '--upgrade') + spec)
+    os.execv(VENV_UPDATE, (VENV_UPDATE,) + orig_args)
 
-    os.execv(VENV_UPDATE, [VENV_UPDATE] + orig_args)
+
+acceptance_tests = dict(
+    upgrade='''
+change requirements.d/venv-update
+use venv-update, shouldn't use old code
+    ''',
+)
 
 
 def convert(s):
