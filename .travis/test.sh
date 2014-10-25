59896fb6645da936d18a61946980132473d0d271
Buck Golemon <buck@yelp.com>
2014-10-24 17:57:30 -0700

    random ci improvements, ported from abandoned capture-subprocess branch

--- .travis/test.sh
+++ .travis/test.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -ex
-TOP=${TOP:-.}
+TOP=$(readlink -f ${TOP:-.})
 SITEPACKAGES=${SITEPACKAGES:-.}
 PROJECT=venv_update
+NCPU=$(getconf _NPROCESSORS_CONF)
 
 export PYTHONPATH=$(readlink -f $SITEPACKAGES)
 
@@ -10,8 +11,8 @@ python --version
 coverage --version
 py.test --version
 coverage erase
-coverage run --parallel-mode --rcfile=$TOP/.coveragerc \
-    -m pytest "$@" $TOP/tests $SITEPACKAGES/${PROJECT}.py
+py.test "$@" -n $NCPU \
+    --cov-enable --cov-config=$TOP/.coveragerc --cov-report='' \
+    $TOP/tests $SITEPACKAGES/${PROJECT}.py
 coverage combine
-coverage report --fail-under 81  # FIXME: should be 100
-mv .coverage $TOP
+coverage report --fail-under 84  # FIXME: should be 100
--- tests/functional/simple_test.py
+++ tests/functional/simple_test.py
@@ -4,13 +4,14 @@ from py._path.local import LocalPath as Path
 import pytest
 
 TOP = Path(__file__) / '../../..'
-SCENARIOS = TOP/'testing/data/scenarios'
+SCENARIOS = TOP/'tests/scenarios'
 
 
-def run(*cmd, **env):
+def run(cmd, *args, **env):
     from pipes import quote
     from subprocess import check_call
 
+    cmd += args
     if env:
         from os import environ
         tmp = env
@@ -25,38 +26,30 @@ def run(*cmd, **env):
     check_call(cmd, env=env)
 
 
-def do_install(pwd, tmpdir):
+def do_install(tmpdir, *args):
+    # we get coverage for free via the (patched) pytest-cov plugin
     run(
-        'coverage',
-        'run',
-        '--parallel-mode',
-        '--rcfile', str(TOP/'.coveragerc'),
-        '-m', 'venv_update',
-        COVERAGE_FILE=str(pwd/'.coverage'),
+        ('venv-update',) + args,
         HOME=str(tmpdir),
     )
 
 
 def test_trivial(tmpdir):
-    pwd = Path('.').realpath()
     tmpdir.chdir()
 
     # Trailing slash is essential to rsync
-    run('rsync', '-a', str(SCENARIOS) + '/trivial/', '.')
-    do_install(pwd, tmpdir)
-
-    pwd.chdir()
+    run(('rsync', '-a', str(SCENARIOS) + '/trivial/', '.'))
+    do_install(tmpdir)
 
 
 # Not yet installed: https://github.com/klrmn/pytest-rerunfailures
 @pytest.mark.flaky(reruns=10)
 def test_second_install_faster(tmpdir):
     """install twice, and the second one should be faster, due to whl caching"""
-    pwd = Path('.').realpath()
     tmpdir.chdir()
 
     # Trailing slash is essential to rsync
-    run('rsync', '-a', str(SCENARIOS) + '/trivial/', '.')
+    run(('rsync', '-a', str(SCENARIOS) + '/trivial/', '.'))
     with open('requirements.txt', 'w') as requirements:
         # An arbitrary package that takes a bit of time to install: twisted
         # Should I make my own fake c-extention just to remove this dependency?
@@ -64,16 +57,18 @@ def test_second_install_faster(tmpdir):
 
     from time import time
     start = time()
-    do_install(pwd, tmpdir)
+    do_install(tmpdir)
     time1 = time() - start
 
     start = time()
-    do_install(pwd, tmpdir)
+    do_install(tmpdir)
     time2 = time() - start
-
-    pwd.chdir()
 
     # second install should be at least twice as fast
     ratio = time1 / time2
     print('%.1fx speedup' % ratio)
     assert ratio / 2
+
+
+def test_arguments():
+    pass  # TODO: anything
