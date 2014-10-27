40f18680c899b5e63fe0272c52bfa8eab6abc0d1
Buck Golemon <workitharder@gmail.com>
2014-10-27 12:21:49 -0700

    py34, pypy3 support

--- tests/functional/simple_test.py
+++ tests/functional/simple_test.py
@@ -6,6 +6,9 @@ import pytest
 TOP = Path(__file__) / '../../..'
 SCENARIOS = TOP/'tests/scenarios'
 
+from sys import version_info
+PY33 = (version_info >= (3, 3))
+
 
 def run(*cmd, **env):
     from subprocess import check_call
@@ -19,6 +22,17 @@ def run(*cmd, **env):
         env = None
 
     check_call(cmd, env=env)
+
+# coverage.py adds some helpful warnings to stderr, with no way to quiet them.
+from re import compile as Regex, MULTILINE
+coverage_warnings_regex = Regex(
+    r'^Coverage.py warning: (Module .* was never imported\.|No data was collected\.)\n',
+    flags=MULTILINE,
+)
+
+
+def strip_coverage_warnings(stderr):
+    return coverage_warnings_regex.sub('', stderr)
 
 
 def do_install(tmpdir, *args):
@@ -75,8 +89,12 @@ def test_arguments_version(tmpdir, capfd):
 
     assert excinfo.value.returncode == 1
     out, err = capfd.readouterr()
-    lasterr = err.rsplit('\n', 2)[-2]
-    assert lasterr.startswith('IOError: [Errno 2] No such file or directory:')
+    lasterr = strip_coverage_warnings(err).rsplit('\n', 2)[-2]
+    if PY33:
+        errname = 'FileNotFoundError'
+    else:
+        errname = 'IOError'
+    assert lasterr.startswith(errname + ': [Errno 2] No such file or directory:')
     assert lasterr.endswith("/activate_this.py'")
 
     out = out.split('\n')
@@ -90,13 +108,29 @@ def test_arguments_system_packages(tmpdir, capfd):
     do_install(tmpdir, '--system-site-packages')
     out, err = capfd.readouterr()  # flush buffers
 
-    run(str(tmpdir.join('virtualenv_run', 'bin', 'python')), '-c', '''\
+    run('virtualenv_run/bin/python', '-c', '''\
 import sys
 for p in sys.path:
-    if p.startswith(sys.real_prefix) and p.endswith("site-packages"):
-        print p
+    if p.startswith(sys.real_prefix) and p.endswith("-packages"):
+        print(p)
 ''')
     out, err = capfd.readouterr()
-    assert err == ''
+    assert strip_coverage_warnings(err) == ''
     out = out.rstrip('\n')
     assert out and Path(out).isdir()
+
+
+def test_update_while_active(tmpdir, capfd):
+    tmpdir.chdir()
+    run('rsync', '-a', str(SCENARIOS) + '/trivial/', '.')
+
+    do_install(tmpdir)
+    run('virtualenv_run/bin/pip', 'freeze')
+    out, err = capfd.readouterr()
+
+    assert strip_coverage_warnings(err) == ''
+    assert 'mccabe' not in out
+
+    with open('requirements.txt', 'w') as requirements:
+        # An arbitrary small package: mccabe
+        requirements.write('mccabe')
