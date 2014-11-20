24ddfb0118d431211a0f248f292278bf5273c608
Anthony Sottile <asottile@umich.edu>
2014-11-19 16:42:45 -0800

    Fix venv-update when system python does not match virtualenv python

--- tests/functional/relocation_test.py
+++ tests/functional/relocation_test.py
@@ -1,3 +1,9 @@
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import io
+import sys
+
 from py._path.local import LocalPath as Path
 
 from testing import get_scenario, run, venv_update
@@ -13,3 +19,20 @@ def test_is_relocatable(tmpdir):
     pip = 'relocated/bin/pip'
     assert Path(pip).exists()
     run(pip, '--version')
+
+
+def test_is_relocatable_different_python_version(tmpdir):
+    tmpdir.chdir()
+    get_scenario('trivial')
+
+    with io.open('requirements.txt', 'w') as reqs:
+        reqs.write('doge==3.5.0')
+
+    if sys.version_info[:2] == (2, 7):  # pragma: no cover PY27 only
+        python_arg = '--python=python2.6'
+    else:  # pragma: no cover PY26 only
+        python_arg = '--python=python2.7'
+
+    venv_update(python_arg)
+
+    run('sh', '-c', '. virtualenv_run/bin/activate && doge --help')
