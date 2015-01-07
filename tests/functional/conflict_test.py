dfa7c1798f63ad675189c8180b5ace2bc6f3d8a6
Buck Golemon <workitharder@gmail.com>
2015-01-07 15:53:28 -0800

    strip coverage warnings :(

--- tests/functional/conflict_test.py
+++ tests/functional/conflict_test.py
@@ -5,26 +5,26 @@ from subprocess import CalledProcessError
 
 import pytest
 
-from testing import requirements, run, uncolor, venv_update
+import testing as T
 
 
 def test_conflicting_reqs(tmpdir):
     tmpdir.chdir()
-    requirements('''
+    T.requirements('''
 # flake8 2.2.5 requires mccabe>=0.2.1, so this isn't satisfiable
 flake8==2.2.5
 mccabe==0.2
 ''')
 
     with pytest.raises(CalledProcessError) as excinfo:
-        venv_update()
-
+        T.venv_update()
     assert excinfo.value.returncode == 1
-
     out, err = excinfo.value.result
+
+    err = T.strip_coverage_warnings(err)
     assert err == ''
 
-    out = uncolor(out)
+    out = T.uncolor(out)
     assert '''
 Successfully installed flake8 mccabe pyflakes pep8 setuptools
 Cleaning up...
@@ -37,11 +37,11 @@ Something went wrong! Sending 'virtualenv_run' back in time, so make knows it's 
 def test_multiple_issues(tmpdir):
     # Make it a bit worse. The output should show all three issues.
     tmpdir.chdir()
-    requirements('flake8==2.2.5')
-    venv_update()
+    T.requirements('flake8==2.2.5')
+    T.venv_update()
 
-    run('./virtualenv_run/bin/pip', 'uninstall', '--yes', 'pyflakes')
-    requirements('''
+    T.run('./virtualenv_run/bin/pip', 'uninstall', '--yes', 'pyflakes')
+    T.requirements('''
 # flake8 2.2.5 requires mccabe>=0.2.1 and pep8>=1.5.7, so this isn't satisfiable
 flake8==2.2.5
 mccabe==0.2
@@ -49,14 +49,14 @@ pep8==1.0
 ''')
 
     with pytest.raises(CalledProcessError) as excinfo:
-        venv_update()
-
+        T.venv_update()
     assert excinfo.value.returncode == 1
-
     out, err = excinfo.value.result
+
+    err = T.strip_coverage_warnings(err)
     assert err == ''
 
-    out = uncolor(out)
+    out = T.uncolor(out)
     assert '''
 Successfully installed mccabe pep8 setuptools
 Cleaning up...
@@ -65,4 +65,4 @@ Error: version conflict: mccabe 0.2 <-> mccabe>=0.2.1 (from flake8==2.2.5 (from 
 Error: version conflict: pep8 1.0 <-> pep8>=1.5.7 (from flake8==2.2.5 (from -r requirements.txt (line 3)))
 
 Something went wrong! Sending 'virtualenv_run' back in time, so make knows it's invalid.
-''' in uncolor(out)
+''' in out
