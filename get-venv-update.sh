0cfd62b02cfefc61924b6db79aa835655c6899e8
Buck Golemon <buck@yelp.com>
2016-03-02 09:04:42 -0800

    get-venv-update: handle wget vs curl

--- get-venv-update.sh
+++ get-venv-update.sh
@@ -1,12 +1,31 @@
-#!/bin/sh
-set -eu
+#!/bin/bash
+set -Eeu
+trap 'echo "[31mFAILED[m (code $?)"' ERR
 
-mkdir -p ./bin
-curl --silent https://raw.githubusercontent.com/Yelp/venv-update/development/venv_update.py --output bin/venv-update
-chmod 755 bin/venv-update
+colorize() {
+    echo "[01;36m>[m [01;33m$@[m" >&2
+    "$@"
+}
+canhas(){
+    command -v "$1" >/dev/null
+}
+download() {
+    if canhas curl; then
+        colorize curl "$1" --output "$2"
+    elif canhas wget; then
+        colorize wget "$1" --output-document "$2"
+    else
+        echo "Could not find 'curl', nor 'wget'."
+        exit 1
+    fi
+}
 
-echo 'Oh, hi!
-I have installed `venv-update` to your bin/ directory.
+colorize mkdir -p ./bin
+download https://github.com/Yelp/venv-update/raw/stable/venv_update.py bin/venv-update
+colorize chmod 755 bin/venv-update
+
+echo '
+I have just installed `venv-update` to your bin/ directory.
 You should commit it to your source control.
 
 To install python requirements from ./requirements.txt to ./venv/,
