bfb1a49574560ac1683e931cd08c3c38def60e8a
Buck Golemon <buck@yelp.com>
2016-03-01 17:55:58 -0800

    new script: get-venv-update.sh

--- get-venv-update.sh
+++ get-venv-update.sh
@@ -1,4 +1,19 @@
-#!/bin/bash
-set -eux
+#!/bin/sh
+set -eu
 
-curl 
+mkdir -p ./bin
+curl --silent https://raw.githubusercontent.com/Yelp/venv-update/development/venv_update.py --output bin/venv-update
+chmod 755 bin/venv-update
+
+echo 'Oh, hi!
+I have installed `venv-update` to your bin/ directory.
+You should commit it to your source control.
+
+To install python requirements from ./requirements.txt to ./venv/,
+simply run: 
+
+    ./bin/venv-update
+
+Please see the user documentation at:
+    http://venv-update.rtfd.org/en/stable/
+'
