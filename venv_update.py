1998c4dd3961e8bd5cd833808c3699a3dd74aadc
Chris Kuehl <ckuehl@yelp.com>
2016-09-29 16:57:59 -0700

    Fix pre-commit

--- venv_update.py
+++ venv_update.py
@@ -163,7 +163,7 @@ def exec_(argv):  # never returns
     """Wrapper to os.execv which shows the command and runs any atexit handlers (for coverage's sake).
     Like os.execv, this function never returns.
     """
-    ## info('EXEC' + colorize(argv))  # TODO: debug logging by environment variable
+    # info('EXEC' + colorize(argv))  # TODO: debug logging by environment variable
 
     # in python3, sys.exitfunc has gone away, and atexit._run_exitfuncs seems to be the only pubic-ish interface
     #   https://hg.python.org/cpython/file/3.4/Modules/atexitmodule.c#l289
