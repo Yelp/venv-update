0d9b50c29d41c573575cd9c89603574978eea4ce
Anthony Sottile <asottile@umich.edu>
2017-01-19 12:41:10 -0800

    v2.0.0rc2

--- venv_update.py
+++ venv_update.py
@@ -59,7 +59,7 @@ from os.path import exists
 from os.path import join
 from subprocess import CalledProcessError
 
-__version__ = '2.0.0rc1'
+__version__ = '2.0.0rc2'
 DEFAULT_VIRTUALENV_PATH = 'venv'
 DEFAULT_OPTION_VALUES = {
     'venv=': (DEFAULT_VIRTUALENV_PATH,),
