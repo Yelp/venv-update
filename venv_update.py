4baae0d62fb501be386f9426cf4be9010727a89e
Kaisen Chen <kaisen@yelp.com>
2021-05-26 10:49:16 -0700

    Use sufficiently newer version of virtualenv (matches minimum bound in pre-commit)

--- venv_update.py
+++ venv_update.py
@@ -213,7 +213,7 @@ def exec_scratch_virtualenv(args):
         # TODO: do we allow user-defined override of which version of virtualenv to install?
         # https://github.com/Yelp/venv-update/issues/231 virtualenv 20+ is not supported.
         tmp = scratch.src + '.tmp'
-        run((scratch_python, '-m', 'pip.__main__', 'install', 'virtualenv>=20', '--target', tmp))
+        run((scratch_python, '-m', 'pip.__main__', 'install', 'virtualenv>=20.0.8', '--target', tmp))
         from os import rename
         rename(tmp, scratch.src)
 
